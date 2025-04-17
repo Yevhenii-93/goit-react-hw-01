218f5aad8d4da27c4302f8f618a25e3e6f790cf2
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-04-18 02:55:56 +0300

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 
-import Profile from "./components/profile/Profile";
 import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import FriendList from "./components/FinedList/FriendList";
+import Profile from "./components/Profile/Profile";
 
 import userData from "./data/userData.json";
 import friends from "./data/friends.json";
